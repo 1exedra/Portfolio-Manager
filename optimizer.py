44cf116bcf2358d25871b7f89a1e7a51a7accbc4
1exedra <exedreye@gmail.com>
2024-06-04 08:08:46 +0300

    Create optimizer.py

---
+++ optimizer.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import yfinance as yf
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import cvxpy as cp
+import matplotlib.pyplot as plt
+from google.colab import files
